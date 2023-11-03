44c72755ff4ab12f0c9b6ab0d471cb7f7ffe8b39
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-03 16:01:03 +0700

    fix: use cors middlewares

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// server configuration
+const app = express();
+const port = 3001;
+
+// importr library CORS
+const cors = require("cors");
+
+// use CORS
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// import router mahasiswa
+const mahasiswaRouter = require("./routes/mahasiswa.route");
+
+// use Router middleware
+app.use('/api/mahasiswa', mahasiswaRouter);
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
